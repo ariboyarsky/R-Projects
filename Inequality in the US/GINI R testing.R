3de4e0a04184cccfc32d907d0d91f6c08935dd20
Ari Boyarsky <ariboyarsky@gwmail.gwu.edu>
2016-10-27 12:29:46 -0400

    make some visual changes and add more info

--- Inequality in the US/GINI R testing.R
+++ Inequality in the US/GINI R testing.R
@@ -1,34 +1,38 @@
 rm(list = ls())
 
+
+US_GINI_Data <- read.csv("US Census Data Intra-US GINI/ACS_15_1YR_B19083_with_ann.csv")
+
+suppressMessages(library(rgeos))
 library(ggplot2)
 library(rgeos)
 library(plyr)
 library(scales)
-library(maptools)
 require(graphics)
 library(ggthemes)
 library(colorspace)
 
-US_GINI_Data <- read.csv("~/College/Senior/Sem 1/Modeling and Visulizing Politics/RVisulization/US Census Data Intra-US GINI/ACS_15_1YR_B19083_with_ann.csv")
+# read csv
+US_GINI_Data <- read.csv("US Census Data Intra-US GINI/ACS_15_1YR_B19083_with_ann.csv")
 
-#state data
+# state data
 states_map <- map_data("state")
 
-#Let's give better names
+# Let's give better names
 colnames(US_GINI_Data) <- c("GEO.id", "GEO.id2", "state", "GINI", "GINI2")
 
-#change to lower case
+# change to lower case
 gini <- as.data.frame(lapply(US_GINI_Data, tolower), stringsAsFactors = FALSE)
-#we have to trasform GINI to num
+# we have to trasform GINI to num
 gini <- transform(gini, GINI = as.numeric(GINI))
 
-#megre map data and GINI data
+# megre map data and GINI data
 gini_map <- merge(states_map, gini, by.x = "region", by.y = "state")
 
-#create map, g
+# create map, g
 g <- ggplot(data=gini_map, mapping = aes(x=long, y=lat, group=group))+
   geom_polygon(aes(fill=GINI))+
-  geom_path()
+  geom_path() + coord_map("mercator")
 
 remove_axes <- theme(
   axis.text = element_blank(),
@@ -38,10 +42,11 @@ remove_axes <- theme(
   panel.grid = element_blank(),
   axis.title = element_blank()
 )
-
+# add title
 g <- g+remove_axes+ggtitle('GINI Accross the United States')
-g <- g+scale_fill_distiller(name="GINI", palette = "YlGnBu", breaks = pretty_breaks(n = 5))
-g
+# change data color encoding
+g <- g+scale_fill_distiller(name="GINI", palette = "YlGn", breaks = pretty_breaks(n = 5))
 
-gini
+# print graph
+g
 
